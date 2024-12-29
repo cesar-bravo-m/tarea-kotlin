fa3a3bfd577d7740ac15137edb224943ade52c49
César Bravo <cbravo@bcyt.cl>
2024-12-29 20:19:13 -0300

    Imágenes

--- app/src/main/java/com/example/myapplication/DayRecipeCard.kt
+++ app/src/main/java/com/example/myapplication/DayRecipeCard.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication
 
+import android.graphics.ColorFilter
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
@@ -10,6 +13,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
@@ -77,7 +86,7 @@ fun DayRecipeCard(
                 if (selectedRecipe != null) {
                     Surface(
                         color = MaterialTheme.colorScheme.primaryContainer,
-                        shape = MaterialTheme.shapes.medium
+                        shape = MaterialTheme.shapes.medium,
                     ) {
                         Text(
                             text = "Puntuación: ${selectedRecipe.nutritionalScore}",
@@ -96,7 +105,7 @@ fun DayRecipeCard(
                         .fillMaxWidth()
                         .padding(top = 16.dp),
                     horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.Top
+                    verticalAlignment = Alignment.Top,
                 ) {
                     Column(
                         modifier = Modifier
